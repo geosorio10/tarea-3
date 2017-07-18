39253fe5264b2c028b4a15f79c28b17366f9efe4
German <ge.osorio10@uniandes.edu.co>
2017-07-17 20:10:37 -0500

    sí estaba evolucionando la onda, el paso del tiempo era muy pequeño. Grafica en el tiempo 30 y 60

--- Onda.py
+++ Onda.py
@@ -12,16 +12,16 @@ matriz=np.zeros((n_puntos,n_puntos))
 
 matriz[30,150]=-0.5
 
-
 u_inicial=matriz #forma inicial de la onda
 
 c=1.0
 delta_x=x[1]-x[0]
 delta_y=y[1]-y[0]
-delta_t=0.0005 #con esto se cumple que gamma y beta sean menores de 1
+delta_t=0.100001 #con esto se cumple que gamma y beta sean menores de 1
 
 gamma=(c* delta_t)/delta_x
 beta=(c*delta_t)/delta_y
+
 
 #condiciones de frontera fijas
 
@@ -60,27 +60,53 @@ for j in range(1,n_puntos-1):
 u_pasado=u_inicial.copy()
 u_presente=u_siguiente.copy()
 
-n_tiempo=60
-for l in range(n_tiempo):
-    for i in range(1,n_puntos-1):
-        for j in range(1,n_puntos-1):
-            u_siguiente[j,i]=(gamma**2)*(u_presente[j,i+1]-2.0*u_presente[j,i]+u_presente[j,i-1])+(beta**2)*(u_presente[j+1,i]-2.0*u_presente[j,i]+u_presente[j-1,i])+2.0*u_presente[j,i]-u_pasado[j,i]
-        u_pasado=u_presente.copy()
-        u_presente=u_siguiente.copy()
 
 
-
-
-
-
-
-#grafica
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
 
 xGrid, yGrid=np.meshgrid(y,x)
 
-#ax.plot_wireframe(xGrid, yGrid, u_presente, rstride=1, cstride=1)
+n_tiempo=60
+
+for l in range(n_tiempo):
+    
+    for i in range(1,n_puntos-1):
+        for j in range(1,n_puntos-1):
+            
+            u_siguiente[j,i]=2.0*(1-gamma**2-beta**2)*u_presente[j,i]-u_pasado[j,i]+(gamma**2)*(u_presente[j,i+1]+u_presente[j,i-1])+(beta**2)*(u_presente[j+1,i]+u_presente[j-1,i])
+            
+    u_pasado=u_presente.copy()
+    u_presente=u_siguiente.copy()
+
+    if(l==30):
+        
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        
+        ax.plot_wireframe(xGrid, yGrid, u_presente, rstride=1, cstride=1)
+        
+        ax.set_xlim(14,16)
+        ax.set_ylim(0,6)
+        
+        plt.savefig('30.png')
+        plt.close()
+        
+    if(l==(n_tiempo-1)):
+        
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        
+        ax.plot_wireframe(xGrid,yGrid,u_presente)
+        ax.set_xlim(14,16)
+        ax.set_ylim(0,6)
+        
+        plt.savefig('60.png')
+        plt.close()
+
+        
+
+
+
+
+
 
 
